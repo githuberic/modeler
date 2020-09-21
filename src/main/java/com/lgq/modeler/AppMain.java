194e91dd61a27249029e595e8201255fd6f12485
lgq <liuguoqing@hzxituan.com>
2020-09-22 00:32:43 +0800

    调整appmain

--- src/main/java/com/lgq/modeler/AppMain.java
+++ src/main/java/com/lgq/modeler/AppMain.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ModelerApplication {
+public class AppMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModelerApplication.class, args);
+		SpringApplication.run(AppMain.class, args);
 	}
 
 }
