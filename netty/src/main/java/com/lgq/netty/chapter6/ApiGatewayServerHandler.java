8ccfd85ab3768deb8813e4d94095dbece4f39592
lgq <liugq@weipaitang.com>
2021-07-25 01:25:53 +0800

    chapter6 add readme

--- netty/src/main/java/com/lgq/netty/chapter6/ApiGatewayServerHandler.java
+++ netty/src/main/java/com/lgq/netty/chapter6/ApiGatewayServerHandler.java
@@ -14,9 +14,19 @@ import java.util.concurrent.TimeUnit;
 public class ApiGatewayServerHandler extends ChannelInboundHandlerAdapter {
     ExecutorService executorService = Executors.newFixedThreadPool(8);
 
+    /**
+     * api网关，每次收到请求，无论大小，都构造一个64k的char数组，用于处理和转发消息，如果后端处理慢
+     * 则会导致任务队列挤压，每个任务都会持有一个64k array，挤压多了就会转移到老年代，一旦触发老年代GC耗时较长，就会导致
+     * 系统吞吐降低为0
+     *
+     * @param ctx
+     * @param msg
+     */
+    /*
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ctx.write(msg);
+
         char[] req = new char[64 * 1024];
         executorService.execute(() ->
         {
@@ -27,9 +37,14 @@ public class ApiGatewayServerHandler extends ChannelInboundHandlerAdapter {
                 e.printStackTrace();
             }
         });
-    }
+    }*/
 
-    /*
+    /**
+     * 1：按请求消息大小来初始化char数组
+     * @param ctx
+     * @param msg
+     */
+    @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ctx.write(msg);
         char[] req = new char[((ByteBuf) msg).readableBytes()];
@@ -42,7 +57,7 @@ public class ApiGatewayServerHandler extends ChannelInboundHandlerAdapter {
                 e.printStackTrace();
             }
         });
-    }*/
+    }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
