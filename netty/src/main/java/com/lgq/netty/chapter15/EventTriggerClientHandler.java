94d4561bf26096fa1ca6d5da78093b83b10117c0
lgq <liugq@weipaitang.com>
2021-08-03 00:18:39 +0800

    add chapter15

---
+++ netty/src/main/java/com/lgq/netty/chapter15/EventTriggerClientHandler.java
@@ -0,0 +1,41 @@
+package com.lgq.netty.chapter15;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author lgq
+ */
+public class EventTriggerClientHandler extends ChannelInboundHandlerAdapter {
+    private static AtomicInteger SEQ = new AtomicInteger(0);
+
+    static final String ECHO_REQ = "Hi,welcome to Netty ";
+
+    static final String DELIMITER = "$_";
+
+    static ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            int counter = SEQ.incrementAndGet();
+            if (counter % 10 == 0) {
+                ctx.writeAndFlush(Unpooled.copiedBuffer((ECHO_REQ + DELIMITER).getBytes()));
+            } else
+                ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+        }, 0, 1000, TimeUnit.MILLISECONDS);
+    }
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ netty/src/main/java/com/lgq/netty/chapter15/EventTriggerServerHandler.java
@@ -0,0 +1,37 @@
+package com.lgq.netty.chapter15;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * @author lgq
+ */
+public class EventTriggerServerHandler extends ChannelInboundHandlerAdapter {
+
+    int counter;
+    int readCompleteTimes;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("This is " + ++counter + " times receive client : [" + body + "]");
+        body += "$_";
+        ByteBuf echo = Unpooled.copiedBuffer(body.getBytes());
+        ctx.writeAndFlush(echo);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.fireChannelReadComplete();
+        readCompleteTimes++;
+        System.out.println("This is " + readCompleteTimes + " times receive ReadComplete event.");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();// 发生异常，关闭链路
+    }
+}
