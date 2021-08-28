1dc11ac98ba524468869fabe0165b792aa608791
lgq <liugq@weipaitang.com>
2021-08-28 14:49:58 +0800

    remove useless files

--- src/main/java/com/lgq/modeler/app/html/a.java
+++
@@ -1,7 +0,0 @@
-package com.lgq.modeler.app.html;
-
-/**
- * Created by eric on 2020/1/11.
- */
-public class a {
-}
--- src/main/java/com/lgq/modeler/bk/serialize/a.java
+++
@@ -1,7 +0,0 @@
-package com.lgq.modeler.bk.serialize;
-
-/**
- * Created by eric on 2019/12/27.
- */
-public class a {
-}
