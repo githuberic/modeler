392aaefcb7ec3e75effda3ad1300786980f03e4c
lgq <liugq@weipaitang.com>
2022-01-03 21:41:55 +0800

    add pb7con-WordCountProducerConsumer

---
+++ concurrent/src/main/java/com/lgq/multiplethread/pb7con/WordCountProducerConsumer/Page.java
@@ -0,0 +1,18 @@
+package com.lgq.multiplethread.pb7con.WordCountProducerConsumer;
+
+/**
+ * @author lgq
+ */
+public class Page {
+    public String getTitle() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getText() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isPoisonPill() {
+        return false;
+    }
+}
---
+++ concurrent/src/main/java/com/lgq/multiplethread/pb7con/WordCountProducerConsumer/PoisonPill.java
@@ -0,0 +1,11 @@
+package com.lgq.multiplethread.pb7con.WordCountProducerConsumer;
+
+/**
+ * @author lgq
+ */
+public class PoisonPill extends Page {
+    @Override
+    public boolean isPoisonPill() {
+        return true;
+    }
+}
