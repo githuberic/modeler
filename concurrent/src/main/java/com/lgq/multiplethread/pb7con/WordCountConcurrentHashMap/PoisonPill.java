803e190088ff2c37938546b4d333d2181fa6a3e9
lgq <liugq@weipaitang.com>
2022-01-03 22:10:28 +0800

    add pb7con-WordCountConcurrentHashMap

---
+++ concurrent/src/main/java/com/lgq/multiplethread/pb7con/WordCountConcurrentHashMap/PoisonPill.java
@@ -0,0 +1,11 @@
+package com.lgq.multiplethread.pb7con.WordCountConcurrentHashMap;
+
+/**
+ * @author lgq
+ */
+public class PoisonPill extends Page {
+    @Override
+    public boolean isPoisonPill() {
+        return true;
+    }
+}
