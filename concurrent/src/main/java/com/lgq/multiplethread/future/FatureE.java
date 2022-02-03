2b41686980af9b308eeb6a2fb8a36b1f3d9aa9a5
lgq <liugq@weipaitang.com>
2022-02-03 22:58:09 +0800

    add compile-successful

---
+++ concurrent/src/main/java/com/lgq/multiplethread/future/FatureE.java
@@ -0,0 +1,96 @@
+package com.lgq.multiplethread.future;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author lgq
+ */
+public class FatureE {
+    public static final int SLEEP_GAP = 500;
+
+    public static String getCurThreadName() {
+        return Thread.currentThread().getName();
+    }
+
+    static class HotWaterJob implements Callable<Boolean> //①
+    {
+        @Override
+        public Boolean call() throws Exception //②
+        {
+            try {
+                System.out.println("洗好水壶");
+                System.out.println("灌上凉水");
+                System.out.println("放在火上");
+
+                //线程睡眠一段时间，代表烧水中
+                Thread.sleep(SLEEP_GAP);
+                System.out.println("水开了");
+            } catch (InterruptedException e) {
+                System.out.println(" 发生异常被中断.");
+                return false;
+            }
+            System.out.println(" 烧水工作 运行结束.");
+
+            return true;
+        }
+    }
+
+    static class WashJob implements Callable<Boolean> {
+        @Override
+        public Boolean call() throws Exception {
+            try {
+                System.out.println("洗茶壶");
+                System.out.println("洗茶杯");
+                System.out.println("拿茶叶");
+                //线程睡眠一段时间，代表清洗中
+                Thread.sleep(SLEEP_GAP);
+                System.out.println("洗完了");
+            } catch (InterruptedException e) {
+                System.out.println(" 清洗工作 发生异常被中断.");
+                return false;
+            }
+            System.out.println(" 清洗工作  运行结束.");
+            return true;
+        }
+    }
+
+    public static void drinkTea(boolean waterOk, boolean cupOk) {
+        if (waterOk && cupOk) {
+            System.out.println("泡茶喝");
+        } else if (!waterOk) {
+            System.out.println("烧水失败，没有茶喝了");
+        } else if (!cupOk) {
+            System.out.println("杯子洗不了，没有茶喝了");
+        }
+    }
+
+    public static void main(String args[]) {
+        Callable<Boolean> hJob = new HotWaterJob();
+        FutureTask<Boolean> hTask = new FutureTask<>(hJob);
+        Thread hotThread = new Thread(hTask, "** 烧水-Thread");
+
+        Callable<Boolean> wJob = new WashJob();
+        FutureTask<Boolean> wTask = new FutureTask<>(wJob);
+        Thread washThread = new Thread(wTask, "$$ 清洗-Thread");
+        hotThread.start();
+        washThread.start();
+
+        Thread.currentThread().setName("主线程");
+
+        try {
+            boolean waterOk = hTask.get();
+            boolean cupOk = wTask.get();
+
+//            hThread.join();
+//            washThread.join();
+            drinkTea(waterOk, cupOk);
+        } catch (InterruptedException e) {
+            System.out.println(getCurThreadName() + "发生异常被中断.");
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println(getCurThreadName() + " 运行结束.");
+    }
+}
