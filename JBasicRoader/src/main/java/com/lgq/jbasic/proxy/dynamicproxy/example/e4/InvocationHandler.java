107d2cfe414ee951b71ef98d94b9d6707eecd136
lgq <liugq@weipaitang.com>
2022-02-10 14:30:08 +0800

    add dynamic-proxy-e4

---
+++ JBasicRoader/src/main/java/com/lgq/jbasic/proxy/dynamicproxy/example/e4/InvocationHandler.java
@@ -0,0 +1,18 @@
+package com.lgq.jbasic.proxy.dynamicproxy.example.e4;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author lgq
+ */
+public interface InvocationHandler {
+    /**
+     * 用户自定义逻辑
+     *
+     * @param m     被执行方法
+     * @param args  被执行方法参数
+     * @return
+     * @throws Throwable
+     */
+    Object invoke(Method m, Object[] args) throws Throwable;
+}
