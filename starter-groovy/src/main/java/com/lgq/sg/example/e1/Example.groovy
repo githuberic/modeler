5d632806faa6a8b97cef0493e7565b0f5b20015a
lgq <lgq_1314@126.com>
2022-04-03 00:40:33 +0800

    update mudule-groovy

--- starter-groovy/src/main/java/com/lgq/sg/example/e1/Example.groovy
+++ starter-groovy/src/main/java/com/lgq/sg/example/e1/Example.groovy
@@ -18,11 +18,27 @@ class Example {
         println("The value of X1 is " + x1 + "The value of Y1 is " + y1);
 
         range();
+
+        sum(3, 4);
+
+        list();
     }
 
-    private static void range(){
+    private static void range() {
         def range = 5..10;
         println(range)
         println(range.get(2))
+    }
+
+    private static void sum(int x, int y = 6) {
+        int sum = x + y;
+        println(sum)
+    }
+
+    private static void list(){
+        def list = []
+        def list2 = [1,2,3,4]
+        list2.add(12)
+        println list.size()
     }
 }
